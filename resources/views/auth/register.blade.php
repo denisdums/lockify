97982ed0e72e64fde6b743addb897b426c1ed7ec
Gokoura <dumontdd59@gmail.com>
2020-05-25 17:08:28 +0200

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,10 +2,12 @@
 
 @section('content')
 
-    <img src="/assets/images/illustrationRegister.svg" alt="Illustration Register">
+    <img src="/assets/images/illustrationRegister.svg" alt="Illustration Register" class="illustration_register">
 
-    <a href="/register_client" class="btn purple">JE SUIS CLIENT</a>
-    <a href="/register_entrepreneur" class="btn linealpurple">JE SUIS ENTREPRENEUR</a>
+    <div class="btn_box">
+        <a href="/register_client" class="btn purple">JE SUIS CLIENT</a>
+        <a href="/register_entrepreneur" class="btn linealpurple">JE SUIS ENTREPRENEUR</a>
+    </div>
 
 
 
