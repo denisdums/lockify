874633fff93e2e0a428a0b5f39c8df9425bf5b39
Gokoura <dumontdd59@gmail.com>
2020-05-27 11:03:43 +0200

    mise en forme form

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,5 +6,30 @@ $(document).ready(function(){
     },2000)
 
 
+    //register client
+    $('#register_first_next').click(function () {
+        $('.part_one').css('display','none');
+        $('.part_two').css('display','block');
+        $('.timeline_client').css('width','66%');
+    });
+
+    $('#register_first_previous').click(function () {
+        $('.part_one').css('display','block');
+        $('.part_two').css('display','none');
+        $('.timeline_client').css('width','33%');
+    });
+
+    $('#register_second_next').click(function () {
+        $('.part_two').css('display','none');
+        $('.part_three').css('display','block');
+        $('.timeline_client').css('width','100%');
+    });
+
+    $('#register_second_previous').click(function () {
+        $('.part_two').css('display','block');
+        $('.part_three').css('display','none');
+        $('.timeline_client').css('width','66%');
+    });
+
 
 });
