6283de88efd188b67144d1ca20bad2a67f17993c
Gokoura <dumontdd59@gmail.com>
2020-05-28 14:11:34 +0200

    tab metiers

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,37 +11,47 @@
 
 
 @section('content')
+    @php
+    $metiers=[
+        'antiquaire' => 'antiquaire.jpg',
+        'barbier' => 'barbier.jpg',
+        'bijoutier' => 'bijoutier.jpg',
+        'boulangerie' => 'boulangerie.jpg',
+        'charcuterie' => 'charcuterie.jpg',
+        'chocolaterie' => 'chocolaterie.jpg',
+        'coiffeur' => 'coiffeur.jpg',
+        'cordonier' => 'cordonier.jpg',
+        'couturière' => 'couturiere.jpg',
+        'épicerie' => 'epicerie.jpg',
+        'ésthéticienne' => 'estheticienne.jpg',
+        'fleuriste' => 'fleuriste.jpg',
+        'fromagerie' => 'fromagerie.jpg',
+        'garagiste' => 'garagiste.jpg',
+        'maroquinier' => 'maroquinier.jpg',
+        'opticien' => 'opticien.jpg',
+        'parfumerie' => 'parfumerie.jpg',
+        'pressing' => 'pressing.jpg',
+        'pressing' => 'pressing.jpg',
+        'prêt à porter' => 'pret-a-porter.jpg',
+        'réparateur de vélo' => 'reparateur de velo.jpg',
+        'taxidermiste' => 'taxidermiste.jpg',
+
+    ]
+    @endphp
     <div class="template_grid">
-        <a href="" class="grid1"><img src="assets/images/metiers/antiquaire.jpg" alt="antiquaire"><span>Antiquaire</span></a>
-        <a href="" class="grid2"><img src="assets/images/metiers/barbier.jpg" alt="barbier"><span>barbier</span></a>
-        <a href="" class="grid3"><img src="assets/images/metiers/bijoutier.jpg" alt="bijoutier"><span>bijoutier</span></a>
-        <a href="" class="grid4"><img src="assets/images/metiers/boulangerie.jpg" alt="boulangerie"><span>boulangerie</span></a>
-        <a href="" class="grid5"><img src="assets/images/metiers/charcuterie.jpg" alt="charcuterie"><span>charcuterie</span></a>
-        <a href="" class="grid6"><img src="assets/images/metiers/chocolaterie.jpg" alt="chocolaterie"><span>chocolaterie</span></a>
-        <a href="" class="grid7"><img src="assets/images/metiers/coiffeur.jpg" alt="coiffeur"><span>coiffeur</span></a>
-        <a href="" class="grid8"><img src="assets/images/metiers/cordonier.jpg" alt="cordonier"><span>cordonier</span></a>
-        <a href="" class="grid9"><img src="assets/images/metiers/couturiere.jpg" alt="couturière"><span>couturière</span></a>
+    @php($i=0)
+    @foreach ($metiers as $key => $value)
+            @php($i++)
+            <a href="" class="grid{{$i}}"><img src="assets/images/metiers/{{$value}}" alt="{{$key}}"><span>{{$key}}</span></a>
+        @php
+          if($i %9 ==0){
+            $i =0;
+            echo "</div><div class='template_grid'>";
+        }
+        @endphp
+
+    @endforeach
     </div>
-
-    <div class="template_grid">
-        <a href="" class="grid1"><img src="assets/images/metiers/epicerie.jpg" alt="epicerie"><span>épicerie</span></a>
-        <a href="" class="grid2"><img src="assets/images/metiers/estheticienne.jpg" alt="estheticienne"><span>estheticienne</span></a>
-        <a href="" class="grid3"><img src="assets/images/metiers/fleuriste.jpg" alt="fleuriste"><span>fleuriste</span></a>
-        <a href="" class="grid4"><img src="assets/images/metiers/fromagerie.jpg" alt="fromagerie"><span>fromagerie</span></a>
-        <a href="" class="grid5"><img src="assets/images/metiers/garagiste.jpg" alt="garagiste"><span>garagiste</span></a>
-        <a href="" class="grid6"><img src="assets/images/metiers/maroquinier.jpg" alt="maroquinier"><span>maroquinier</span></a>
-        <a href="" class="grid7"><img src="assets/images/metiers/opticien.jpg" alt="opticien"><span>opticien</span></a>
-        <a href="" class="grid8"><img src="assets/images/metiers/parfumerie.jpg" alt="parfumerie"><span>parfumerie</span></a>
-        <a href="" class="grid9"><img src="assets/images/metiers/pressing.jpg" alt="pressing"><span>pressing</span></a>
-    </div>
-
-    <div class="template_grid">
-        <a href="" class="grid1"><img src="assets/images/metiers/pret-a-porter.jpg" alt="pret-a-porter"><span>Prêt à porter</span></a>
-        <a href="" class="grid2"><img src="assets/images/metiers/reparateur de velo.jpg" alt="reparateur de velo"><span>réparateur de vélo</span></a>
-        <a href="" class="grid3"><img src="assets/images/metiers/taxidermiste.jpg" alt="taxidermiste"><span>taxidermiste</span></a>
-    </div>
-
-
 
 
 @endsection
