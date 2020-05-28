2cd442292f0ed5f072436011a43435f46998129f
Gokoura <dumontdd59@gmail.com>
2020-05-28 08:35:53 +0200

    grid template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,28 @@
 
 
 @section('content')
+    <div class="template_grid">
+        <div class="grid1">a</div>
+        <div class="grid2">a</div>
+        <div class="grid3">a</div>
+        <div class="grid4">a</div>
+        <div class="grid5">a</div>
+        <div class="grid6">a</div>
+        <div class="grid7">a</div>
+        <div class="grid8">a</div>
+        <div class="grid9">a</div>
+    </div>
+
+    <div class="template_grid">
+        <div class="grid1">a</div>
+        <div class="grid2">a</div>
+        <div class="grid3">a</div>
+        <div class="grid4">a</div>
+        <div class="grid5">a</div>
+        <div class="grid6">a</div>
+        <div class="grid7">a</div>
+        <div class="grid8">a</div>
+        <div class="grid9">a</div>
+    </div>
 
 @endsection
