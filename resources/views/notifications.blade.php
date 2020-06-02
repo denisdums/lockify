b62dd5ece687a3c7ac1063021239185d269814d0
denisdums <dumontdd59@gmail.com>
2020-06-02 15:53:52 +0200

    jsp

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -14,7 +14,7 @@
         </div>
 
         <div class="contenu_notif">
-            <span class="text_notif"><b>Léo Delplanque</b> a annuler un rendez-vous pour <b>Boucherie Couteau</b> le 23/05/2020 à 14h. </span>
+            <span class="text_notif"><b>Léo Delplanque</b> a ajouté  <b>Denis Dumont</b> dans la famille. </span>
             <span class="date_notif">10H37</span>
         </div>
     </div>
@@ -25,7 +25,7 @@
         </div>
 
         <div class="contenu_notif">
-            <span class="text_notif"><b>Léo Delplanque</b> a pris un rendez-vous pour <b>Coiffeur Ciseaux</b> le 23/05/2020 à 17h. </span>
+            <span class="text_notif"><b>Léo Delplanque</b> a pris un rendez-vous pour <b>Coiffeur Ciseaux</b> le 02/06/2020 à 17h. </span>
             <span class="date_notif">9H16</span>
         </div>
     </div>
@@ -34,16 +34,16 @@
 
 
     <div class="date">
-        <span>21/05/2020</span>
+        <span>29/05/2020</span>
         <hr>
     </div>
 
-    <div class="notif_container">
+    <div class="notif_container no_color">
         <div class="avatar_notif">
             <img src="/uploads/ecb118cc4e7a6fc910d295fd05523cc1.jpeg" alt="img_notif">
         </div>
 
-        <div class="contenu_notif">
+        <div class="contenu_notif " >
             <span class="text_notif"><b>Léo Delplanque</b> a pris un rendez-vous pour <b>Poissonier Citron</b> le 22/05/2020 à 18h. </span>
             <span class="date_notif">19H16</span>
         </div>
